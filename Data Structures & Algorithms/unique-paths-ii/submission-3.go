6e5c6f070adcf46afac69ea3457ea383a5f03b95
ericmiranda7 <47097072+ericmiranda7@users.noreply.github.com>
2026-07-21 06:38:34 +0530

    Add: unique-paths-ii - submission-3

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-3.go
@@ -0,0 +1,16 @@
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	currRow, prevRow := make([]int, len(obstacleGrid[0])+1), make([]int, len(obstacleGrid[0])+1)
+	currRow[len(obstacleGrid[0])] = 1
+
+	for i := len(obstacleGrid)-1; i >= 0; i-- {
+		for j := len(obstacleGrid[0])-1; j >= 0; j-- {
+			if obstacleGrid[i][j] == 0 {
+				currRow[j] = prevRow[j] + currRow[j+1]
+			}
+		}
+		prevRow = currRow
+		currRow = make([]int, len(obstacleGrid[0])+1)
+	}
+
+	return prevRow[0]
+}
